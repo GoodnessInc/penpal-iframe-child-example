73481ad76a5c2d811f20e1d2b75ceaceb9e60d06
Robert Reinhard <robert.reinhard@goodness.inc>
2024-10-29 09:50:37 -0700

    Adding a child API example

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,18 @@
 import './style.css'
 import { connectToParent } from "penpal";
 
+// Define the child API
+const childApi: ChildApi = {
+
+  // Example of receiving a customer ID from the parent iframe
+  customerId: (customerId: string) => {
+    console.log("Customer ID from parent", customerId);
+  }
+};
+
 // Connect to the parent iframe
 const connection = connectToParent<ParentApi>({
-  methods: {},
+  methods: childApi,
 });
 
 // Expose a function to send an example event to the parent iframe
@@ -27,6 +36,11 @@ type BehaviorEvent = {
   event_value?: string | null;
   event_location?: string | null;
   click_url?: string | null;
+};
+
+// The API that we're exposing to the parent window
+type ChildApi = {
+  customerId: (customerId: string) => void;
 };
 
 // Make typescript chill about adding the exposed method
